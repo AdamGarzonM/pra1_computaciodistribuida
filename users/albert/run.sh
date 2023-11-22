1687dd7dd03332bccc5471c321fce80f24180fa9
AdamGarzonM <damiegm12@gmail.com>
2023-11-22 10:29:40 -0500

    se queixa del connect

--- users/albert/run.sh
+++ users/albert/run.sh
@@ -1,3 +1,12 @@
-exec python3 heatpump.py & 
-exec python3 lightbulb.py &
-exec python3 presencesensor.py;
+sleep 2;
+
+# exec python3 heatpump.py & 
+# exec python3 lightbulb.py & 
+# exec python3 presencesensor.py &
+# exec python3 temperaturesensor.py;
+
+python3 heatpump.py; 
+python3 lightbulb.py;  
+python3 presencesensor.py; 
+python3 temperaturesensor.py;
+
