7a3e5482c8dfbbfde0817a88d786aa731ffa3d61
Adàm GMorales <damiegm12@gmail.com>
2023-11-24 23:34:59 +0100

    coses maques influx funciona

--- cloud/save/save.py
+++ cloud/save/save.py
@@ -1,23 +1,37 @@
 from kafka import KafkaConsumer
+#from kafka import NoBrokersAvailable
 from json import loads
 from time import sleep
+import os
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+db_user = os.environ.get("DOCKER_INFLUXDB_INIT_USERNAME")
+db_pass = os.environ.get("DOCKER_INFLUXDB_INIT_PASSWORD")
+db_org = os.environ.get("DOCKER_INFLUXDB_INIT_ORG")
+db_bucket = os.environ.get("DOCKER_INFLUXDB_INIT_BUCKET")
+db_token = os.environ.get("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN")
+db_hostname = os.environ.get("DOCKER_INFLUXDB_HOSTNAME")
 
 kafka_topic = 'topic' 
+while True:
+    try:
+        save_consumer = KafkaConsumer(
+            kafka_topic,
+            bootstrap_servers = ['kafka : 9092'],
+            group_id = 'save_service',
+            auto_offset_reset = 'latest',
+            enable_auto_commit = True,
+            value_deserializer = lambda x: loads(x.decode('utf-8'))
+        )
 
-save_consumer = KafkaConsumer(
-    kafka_topic,
-    bootstrap_servers = ['kafka : 9092'],
-    group_id = 'save_service',
-    auto_offset_reset = 'latest',
-    enable_auto_commit = True,
-    value_deserializer = lambda x: loads(x.decode('utf-8'))
-)
-
-
-
-if __name__ == "__main__":
-    print("SAVE")
-    for message in save_consumer:
-        #message = message.value
-        print(f"SAVE recieved message: {message}")
-    
+        if __name__ == "__main__":
+            print("SAVE")
+            influxToken = "s2d3rf67ren42i0gg666er9"
+            for message in save_consumer:
+                #value='25/1700859867.9161658/temperature'
+                message = message.value.split("/")
+                print(f"SAVE recieved message: {message}")
+    except: #NoBrokersAvailable
+        sleep(1)
+        pass
--- gateway/gateway.py
+++ gateway/gateway.py
@@ -7,18 +7,18 @@ import os
 kafka_producer = KafkaProducer(
     bootstrap_servers='kafka:9092',
     value_serializer=lambda x: dumps(x).encode('utf-8'),
-    api_version=(0,11,5),
+    api_version=(0,11,5), # no se si es necessari
 )
 
 
 def on_message(client, userdata, msg):
     data = msg.payload.decode()
-    print(f"GGateway recieved payload: {data}")
-    data_parts = data.split('/')
-    data_kind = data_parts[2]
-    #data = data_parts[0] + data_parts[1]
+    print(f"Gateway recieved payload: {data}")
+    # data_parts = data.split('/')
+    # data_kind = data_parts[2]
+    #data = data_parts[0] + "/" + data_parts[1]
     #kafka_producer.send(data_kind, data)
-    kafka_producer.send("topic", data)
+    kafka_producer.send("topic", data) #TODO: topic
 
 if __name__ == "__main__":
     broker = os.environ.get("BROKER")
